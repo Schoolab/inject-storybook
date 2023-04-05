ba73eadb5e8e37673ced8c2346f0a8b8bec3af46
terenceIrv <terence@theschoolab.com>
2023-04-05 15:58:12 +0200

    add expert overview

--- stories/ProjectOverview.stories.js
+++ stories/ProjectOverview.stories.js
@@ -1,11 +1,11 @@
 import { createProjectOverview } from './layouts/projectOverview.js';
 
 export default {
-    title: 'Pages/Project/Overview',
+    title: 'Pages/Project/ProjectOverview',
     parameters: {
         // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
         layout: 'fullscreen',
       },
 };
 
-export const Overview = () => createProjectOverview();
+export const ProjectOverview = () => createProjectOverview();
