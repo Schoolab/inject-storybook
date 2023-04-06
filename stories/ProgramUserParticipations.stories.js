a7215cec1a0fae2cf2323be9e9c6004de21fe22e
terenceIrv <terence@theschoolab.com>
2023-04-06 18:01:14 +0200

    add program user participations

---
+++ stories/ProgramUserParticipations.stories.js
@@ -0,0 +1,11 @@
+import { createProgramUserParticipations } from './layouts/programUserParticipations.js';
+
+export default {
+    title: 'Pages/Program/User/Participations',
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
+        layout: 'fullscreen',
+      },
+};
+
+export const Participations = () => createProgramUserParticipations();
