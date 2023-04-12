21e6c5024d1d42a049500f672359c4c276ce48d6
terenceIrv <terence@theschoolab.com>
2023-04-12 15:55:28 +0200

    add Juries Projects evaluation session

---
+++ stories/layouts/juryProjectsSession.js
@@ -0,0 +1,45 @@
+import navBar from '../components/navBar.html';
+import shortcutBar from '../components/shortcutBar.html';
+import breadcrump from "../components/breadcrump.html";
+import programContent from "../components/jury-projects-session-content.html";
+import subNav  from "../components/subNav.html";
+import applicationTitle from "../components/application-title.html";
+import { script } from "../utils/script.js";
+
+export const createJuryProjectsSession = () => {
+  const applicationUi = document.createElement('div');
+  applicationUi.className = 'application-ui';
+  
+  applicationUi.insertAdjacentHTML('beforeend', navBar);
+
+  const applicationContainer = document.createElement('div');
+  applicationContainer.className = 'application-container';
+
+  applicationUi.appendChild(applicationContainer);
+
+  applicationContainer.insertAdjacentHTML('beforeend',shortcutBar);
+
+  const applicationContent = document.createElement('main');
+  applicationContent.className = 'application-content';
+
+  applicationContainer.appendChild(applicationContent);
+
+  const applicationContentDiv = document.createElement('div');
+  applicationContentDiv.className = 'd-flex flex-column';
+
+  applicationContent.appendChild(applicationContentDiv);
+
+  applicationContentDiv.insertAdjacentHTML('beforeend',breadcrump);
+  applicationContentDiv.insertAdjacentHTML('beforeend',applicationTitle);
+
+  applicationContentDiv.insertAdjacentHTML('beforeend',subNav);
+  const appMainContainer = document.createElement('div');
+  appMainContainer.id ="appMainContainer";
+  appMainContainer.className = 'container-lg container-lg-fluid mb-lg-0 py-7 px-7';
+
+  applicationContentDiv.appendChild(appMainContainer);
+
+  appMainContainer.insertAdjacentHTML('beforeend',programContent);
+  script();
+  return applicationUi;
+};
