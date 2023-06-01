9d02e6ff0d0563de78033daf40104344c19c329c
terenceIrv <terence@theschoolab.com>
2023-06-01 19:43:58 +0200

    add external program landing page

---
+++ stories/layouts/externalProgramLandingPage.js
@@ -0,0 +1,45 @@
+import navBar from '../components/navBar.html';
+import shortcutBar from '../components/shortcutBar.html';
+import breadcrump from "../components/breadcrump.html";
+import projectOverviewContent from "../components/external-program-landing-page-content.html";
+import subNav  from "../components/subNav.html";
+import applicationTitle from "../components/application-title.html";
+import { script } from "../utils/script.js"
+
+export const createExternalProgramLandingPage = () => {
+  const applicationUi = document.createElement('div');
+  applicationUi.className = 'application-ui';
+
+  applicationUi.insertAdjacentHTML('beforeend', navBar);
+
+  const applicationContainer = document.createElement('div');
+  applicationContainer.className = 'application-container';
+
+  applicationUi.appendChild(applicationContainer);
+
+  applicationContainer.insertAdjacentHTML('beforeend',shortcutBar);
+
+  const applicationContent = document.createElement('main');
+  applicationContent.className = 'application-content';
+
+  applicationContainer.appendChild(applicationContent);
+
+  const applicationContentDiv = document.createElement('div');
+  applicationContentDiv.className = 'd-flex flex-column';
+
+  applicationContent.appendChild(applicationContentDiv);
+
+  applicationContentDiv.insertAdjacentHTML('beforeend',breadcrump);
+  applicationContentDiv.insertAdjacentHTML('beforeend',applicationTitle);
+
+  applicationContentDiv.insertAdjacentHTML('beforeend',subNav);
+
+  const appMainContainer = document.createElement('div');
+  appMainContainer.id ="appMainContainer";
+
+  applicationContentDiv.appendChild(appMainContainer);
+
+  appMainContainer.insertAdjacentHTML('beforeend',projectOverviewContent);
+  script();
+  return applicationUi;
+};
