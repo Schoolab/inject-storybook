24e37d7bdb35bbb228beee75f9270a86de663cc7
terenceIrv <terence@theschoolab.com>
2023-09-04 17:58:12 +0400

    add default card

---
+++ stories/programsHub-explore.stories.js
@@ -0,0 +1,11 @@
+import { createProgramHub } from './layouts/programsHub-explore.js';
+
+export default {
+    title: 'Pages/Programs Hub/Explore',
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
+        layout: 'fullscreen',
+      },
+};
+
+export const Explore = () => createProgramHub();
