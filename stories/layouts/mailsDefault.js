e6351455b51ed31fabfe6efeb865e20eea9c3307
Alexandra Jolly <alexandra@alexandras-laptop.home>
2023-10-09 15:50:22 +0200

    add question and flag email template

--- stories/layouts/mailsDefault.js
+++ stories/layouts/mailsDefault.js
@@ -1,10 +1,10 @@
-import projectOverviewContent from "../components/mails-default-content.html";
+import mailsDefaultContent from "../components/mails-default-content.html";
 import { script } from "../utils/script.js";
 
 export const createMailsDefault = () => {
   const applicationUi = document.createElement('div');
 
-  applicationUi.insertAdjacentHTML('beforeend',projectOverviewContent);
+  applicationUi.insertAdjacentHTML('beforeend',mailsDefaultContent);
 
   script();
   return applicationUi;
---
+++ stories/layouts/mailsQuestion.js
@@ -0,0 +1,11 @@
+import mailsQuestionContent from "../components/mails-question-content.html";
+import { script } from "../utils/script.js";
+
+export const createMailsQuestion = () => {
+  const applicationUi = document.createElement('div');
+
+  applicationUi.insertAdjacentHTML('beforeend',mailsQuestionContent);
+
+  script();
+  return applicationUi;
+};
