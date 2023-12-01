eeab799b4f24482b8a8b309be8c1923f25aca599
terenceIrv <terence@theschoolab.com>
2023-12-01 17:33:44 +0400

    add manage setup and forms

---
+++ stories/ManageForm.stories.js
@@ -0,0 +1,11 @@
+import { createManageForm } from './layouts/ManageForm.js';
+
+export default {
+    title: 'Pages/Manage/Forms',
+    parameters: {
+        // More on Story layout: https://storybook.js.org/docs/html/configure/story-layout
+        layout: 'fullscreen',
+      },
+};
+
+export const Forms = () => createManageForm();
--- stories/utils/script.js
+++ stories/utils/script.js
@@ -18,5 +18,37 @@ export const script = () => {
         });
         //dropdown
         $('.dropdown-toggle').dropdown();
-    })    
+        //drag and scroll horizontally
+        const dragScrollH = document.querySelector('.scroll-h');
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+        if (dragScrollH !== null){
+            dragScrollH.addEventListener('mousedown', (e) => {
+                isDown = true;
+                dragScrollH.classList.add('active');
+                startX = e.pageX - dragScrollH.offsetLeft;
+                scrollLeft = dragScrollH.scrollLeft;
+            });
+            dragScrollH.addEventListener('mouseleave', () => {
+                isDown = false;
+                dragScrollH.classList.remove('active');
+            });
+            dragScrollH.addEventListener('mouseup', () => {
+                isDown = false;
+                dragScrollH.classList.remove('active');
+            });
+            dragScrollH.addEventListener('mousemove', (e) => {
+                if (!isDown) return;
+                e.preventDefault();
+                const x = e.pageX - dragScrollH.offsetLeft;
+                const walk = (x - startX) * 3;
+                dragScrollH.scrollLeft = scrollLeft - walk;
+            });
+        }     
+    })  
 };
+
+
+
+
