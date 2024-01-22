3a5682f3b2269dfd6b5c36bb0c0c3321b3d6b9f9
terenceIrv <terence@theschoolab.com>
2024-01-22 17:53:20 +0400

    fix tooltip

--- stories/utils/script.js
+++ stories/utils/script.js
@@ -1,5 +1,21 @@
 export const script = () => {
-    $(function () {        
+    $(function () { 
+        //tooltips
+        var tooltips = $('[data-toggle="tooltip"]');
+        tooltips.tooltip();
+        function disableTooltipInMobile() {
+            tooltips.tooltip('enable');
+            if (window.matchMedia("(max-width: 991.98px)").matches) {
+                tooltips.tooltip('disable');
+            }
+            $('.application-shortcutbar.shortcutbar-expanded a').tooltip('disable')
+            $('.application-shortcutbar.shortcutbar-expanded div').tooltip('disable')
+
+        }
+        disableTooltipInMobile();
+        $(window).resize(function () {
+            disableTooltipInMobile();
+        });       
         // const scrollContainer = document.getElementById('testscroll');
         
         // scrollContainer.addEventListener('mousedown', (e) => {
